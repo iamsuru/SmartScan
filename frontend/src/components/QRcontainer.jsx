2f66832ae2812c3a255034fab48d3bc72a8c4914
iamsuru <suryanshshrivastava6364@gmail.com>
2024-04-22 02:36:22 +0530

    Beta Version 1.0.0

--- frontend/src/components/QRcontainer.jsx
+++ frontend/src/components/QRcontainer.jsx
@@ -33,7 +33,7 @@ function QRcontainer(props) {
 
     return (
         <div className='d-flex flex-column justify-content-center align-items-center'>
-            <QRCodeSVG value={`https://smartscan.onrender.com/upload?ID=${id}&location=${props.uniqueCode}`} />
+            <QRCodeSVG value={`https://smartscanmern.onrender.com/upload?ID=${id}&location=${props.uniqueCode}`} />
             <div className='mt-4'>
                 <label style={{ color: 'blue' }}>Remaining Time: {`${seconds} seconds`}</label>
                 <Progress value={progressValue} size='xs' colorScheme='blue' className='mt-3' />
