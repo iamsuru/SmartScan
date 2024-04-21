67712d0a1ec71413e8db14b50dbff585abdbb919
iamsuru <suryanshshrivastava6364@gmail.com>
2024-04-21 15:22:12 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,22 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', AuthRouter)
 app.use('/api', OtherRouter)
 
-app.listen(2000, () => {
-    console.log('Server running on PORT 2000');
+//Deployment Code
+
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname1, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.end('API running succesfully.')
+    })
+}
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT ${process.env.PORT}`);
 })
+
+// "build": "npm install --legacy-peer-deps && npm install --legacy-peer-deps --prefix frontend && npm run build --prefix frontend"
